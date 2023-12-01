9330d842d3b4ce3bb79484e941d1bcb8b673b6a8
ragg09 <ragunayon@gmail.com>
2023-12-01 16:17:08 +0800

    Project Initial Configuration

---
+++ .github/pull_request_template.md
@@ -0,0 +1,19 @@
+## Task ID
+
+ID: cashow-
+
+## Definition of Done
+
+n/a
+
+## Steps to reproduce
+
+n/a
+
+## Affected Files
+
+n/a
+
+## Notes
+
+n/a
--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+import CounterScreen from './src/screens/CounterScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      {/* <View style={styles.container}>
+        <Text>Counter</Text>
+        <Text>{count} asd</Text>
+        <StatusBar style="auto" />
+      </View> */}
+      <CounterScreen />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
