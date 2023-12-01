40b789cbb5ba3aaac82a45f1efa1a7625b042a84
ragg09 <54803732+ragg09@users.noreply.github.com>
2023-12-01 16:47:50 +0800

    Dev Tools Configuration (#2)

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,51 +1,37 @@
 import { StyleSheet, Text, View, Button } from 'react-native';
-
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
-
 import { decrement, increment } from '../redux/features/counter/counterSlice';
 
-export default function CounterScreen() {
+const CounterScreen = () => {
   const count = useAppSelector((state) => state.counter.value);
-
   const dispatch = useAppDispatch();
-
   return (
     <View style={styles.rootContainer}>
       <Text>Testing {count}</Text>
-
       <View>
         <Button
           onPress={() => dispatch(increment())}
-          title='Add'
-          color='#841584'
-          accessibilityLabel='Learn more about this purple button'
+          title="Add"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
         />
-
         <Button
           onPress={() => dispatch(decrement())}
-          title='minus'
-          color='#841584'
-          accessibilityLabel='Learn more about this purple button'
+          title="minus"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
         />
       </View>
     </View>
   );
-}
+};
+
+export default CounterScreen;
 
 const styles = StyleSheet.create({
   rootContainer: {
+    alignItems: 'center',
     flex: 1,
-
     justifyContent: 'center',
-
-    alignItems: 'center'
   },
-
-  text: {
-    fontSize: 18,
-
-    fontWeight: 'bold',
-
-    color: 'white'
-  }
 });
